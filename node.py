791362d14c1bd1e37545da3f952a81d5fdfe20ac
olivierzq <1374633989@qq.com>
2018-05-24 23:49:17 +0800

    Update node.py

--- node.py
+++ node.py
@@ -37,230 +37,231 @@ storeRT_state = threading.Lock()
 pktRecv_state = threading.Lock()
 
 
+
 class Coord:
     def __init__(self, coordx, coordy):
         self.x = coordx
         self.y = coordy
-        
-def sendHello(PORT):
-	
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-	while True:
-		data = ''
-		network = '<broadcast>'
-		addr = (network, PORT)
-		data = genHelloMsg()
-		s.sendto(data, addr)
-		storeNI_state.acquire()
-		print 'send: ' + data
-		storeNI_state.release()
-		time.sleep(1)
-		
-	s.close()   
-	
+
+def sendHello(PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    while True:
+        data = ''
+        network = '<broadcast>'
+        addr = (network, PORT)
+        data = genHelloMsg()
+        s.sendto(data, addr)
+        storeNI_state.acquire()
+        print 'send: ' + data
+        storeNI_state.release()
+        time.sleep(1)
+
+    s.close()
+
+
 def recvHello(PORT):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	
-	s.bind(('', PORT))
-	
-	while True:
-		data, addr = s.recvfrom(100)
-		storeNI_state.acquire()
-		storeReceiveMsg(data)
-		print routingTable
-		storeNI_state.release()
-		
-		
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    s.bind(('', PORT))
+
+    while True:
+        data, addr = s.recvfrom(100)
+        storeNI_state.acquire()
+        storeReceiveMsg(data)
+        print routingTable
+        storeNI_state.release()
+
 
 def storeReceiveMsg(recvData):
-	# Global Parameters
-	# 
-	data = recvData
-	blockNum = len(data.split(';'))
-	#print ("bnum%d"%blockNum)
-	if data.split(';')[0] == 'Hello':
-		seqNum = data.split(';')[1]
+    # Global Parameters
+    #
+    data = recvData
+    blockNum = len(data.split(';'))
+    # print ("bnum%d"%blockNum)
+    if data.split(';')[0] == 'Hello':
+        seqNum = data.split(';')[1]
 
-		sourceNodeStr = data.split(';')[2]
-		node = data.split(';')[2].split(',')[0]
-		
-		if node == currNode:
-			return
-		print 'data   ' + data
-		nodeCoord = data.split(';')[2].split(',')[1]
-		nodeIP = data.split(';')[2].split(',')[2]
-		if blockNum == 3:
-			addToInNI(node, seqNum, nodeCoord, nodeIP)
+        sourceNodeStr = data.split(';')[2]
+        node = data.split(';')[2].split(',')[0]
 
-		elif blockNum > 3:
-			inNeighborStr = data.split(';')[3]
-			nodeInfo = nodeCoord + ';' + inNeighborStr
-			addToInNI(node, seqNum, nodeInfo, nodeIP)
-			
+        if node == currNode:
+            return
+        print 'data   ' + data
+        nodeCoord = data.split(';')[2].split(',')[1]
+        nodeIP = data.split(';')[2].split(',')[2]
+        if blockNum == 3:
+            addToInNI(node, seqNum, nodeCoord, nodeIP)
+
+        elif blockNum > 3:
+            inNeighborStr = data.split(';')[3]
+            nodeInfo = nodeCoord + ';' + inNeighborStr
+            addToInNI(node, seqNum, nodeInfo, nodeIP)
 
 
-			
-def findNodeInfo(node, num): # 0:Coord, 1:cost, 2:Path
-	nodeInfo = rT.get(node)
-	if nodeInfo != none:
-		infoStr = rT[nodeInfo].split(';')
-		return infoStr[num]
+def findNodeInfo(node, num):  # 0:Coord, 1:cost, 2:Path
+    nodeInfo = rT.get(node)
+    if nodeInfo != none:
+        infoStr = rT[nodeInfo].split(';')
+        return infoStr[num]
+
 
 def addSeqNum(node, seq):
-	global seqT
-	if seqT.get(node, 'None') != 'None':
-		if int(seqT[node].split(' ')[0]) < seq:
-			seqT[node] = str(seq) + ' ' + str(currSeq)
-	else :
-		seqT[node] = str(seq) + ' ' + str(currSeq)
-	
+    global seqT
+    if seqT.get(node, 'None') != 'None':
+        if int(seqT[node].split(' ')[0]) < seq:
+            seqT[node] = str(seq) + ' ' + str(currSeq)
+    else:
+        seqT[node] = str(seq) + ' ' + str(currSeq)
+
 
 def addToInNI(node, seqTime, info, nodeIP):
-	global inNI
-	seq = int(seqTime.split(',')[0])
-	time = seqTime.split(',')[1]
-	coord = info.split(';')[0]
-	x = int(coord.split(' ')[0])
-	y = int(coord.split(' ')[1])
-	dist = math.sqrt((x - currX)**2 + (y - currY)**2)
-	if dist <= radius:
-		if inNI.get(node, 'None') != 'None':
-			nodeSeq = int(seqT[node].split(' ')[0])
-			if seq > nodeSeq:
-				inNI[node] = time + ";" + info
-				addSeqNum(node, seq)
-				addIP(node, nodeIP)
-			
-		else:
-			inNI[node] = time + ";" + info
-			addSeqNum(node, seq)
-			addIP(node, nodeIP)
-		
-def addIP(node, nodeIP):
-	global ipTable
-	ipTable[node] = nodeIP
+    global inNI
+    seq = int(seqTime.split(',')[0])
+    time = seqTime.split(',')[1]
+    coord = info.split(';')[0]
+    x = int(coord.split(' ')[0])
+    y = int(coord.split(' ')[1])
+    dist = math.sqrt((x - currX) ** 2 + (y - currY) ** 2)
+    if dist <= radius:
+        if inNI.get(node, 'None') != 'None':
+            nodeSeq = int(seqT[node].split(' ')[0])
+            if seq > nodeSeq:
+                inNI[node] = time + ";" + info
+                addSeqNum(node, seq)
+                addIP(node, nodeIP)
 
+        else:
+            inNI[node] = time + ";" + info
+            addSeqNum(node, seq)
+            addIP(node, nodeIP)
+
+
+def addIP(node, nodeIP):
+    global ipTable
+    ipTable[node] = nodeIP
 
 
 def updateRoutingTable():
-	global inNI
-	
-	while True:
-		storeNI_state.acquire()
-		
-		tmp = []
-		for key in inNI:
-			tmp.append(key)
-		
-		for key2 in tmp:
-		
-			dealInNIMsg(key2)
-			inNI.pop(key2)
-		scanSeq()
-		storeNI_state.release()
-		time.sleep(1)
-		
+    global inNI
+
+    while True:
+        storeNI_state.acquire()
+
+        tmp = []
+        for key in inNI:
+            tmp.append(key)
+
+        for key2 in tmp:
+            dealInNIMsg(key2)
+            inNI.pop(key2)
+        scanSeq()
+        storeNI_state.release()
+        time.sleep(1)
 
 
 def dealInNIMsg(node):
-	global routingTable
-	nodeName = node
-	nodeInfo = inNI[node]
-	nodeCoord = inNI[node].split(';')[1]
-	nodeTime = int(inNI[node].split(';')[0])
-	nodeCost = calCost(nodeCoord, nodeTime)
-	nodePath = nodeName
-	updateNodeInfo(nodeName, nodeCoord, nodeCost, nodePath)
-	#print (inNI[node])
-	if len(inNI[node].split(';'))>2:
-		nodeNum = len(inNI[node].split(';')[2].split('/'))
-		#print (nodeNum)
-		for i in range(0, nodeNum):
-			currNodeInfo = inNI[node].split(';')[2].split('/')[i]
-			currNodeName = currNodeInfo.split(',')[0]
-			
-			if currNodeName != currNode:
-				currCoord = currNodeInfo.split(',')[1]
-				currNodeX = int(currCoord.split(' ')[0])
-				currNodeY = int(currCoord.split(' ')[1])
-				dist = math.sqrt((currNodeX - currX)**2 + (currNodeY - currY)**2)
-				
-				#print dist
-				if dist <= radius:
-					nodeInRTInfo = routingTable.get(nodeName, 'None')
-					if nodeInRTInfo!='None':
-						currCost = int(currNodeInfo.split(',')[2]) + int(nodeInRTInfo.split(';')[1])
-						currPath = nodeName + currNodeInfo.split(',')[3]
-						updateNodeInfo(currNodeName, currCoord, currCost, currPath)
-	#return
+    global routingTable
+    nodeName = node
+    nodeInfo = inNI[node]
+    nodeCoord = inNI[node].split(';')[1]
+    nodeTime = int(inNI[node].split(';')[0])
+    nodeCost = calCost(nodeCoord, nodeTime)
+    nodePath = nodeName
+    updateNodeInfo(nodeName, nodeCoord, nodeCost, nodePath)
+    # print (inNI[node])
+    if len(inNI[node].split(';')) > 2:
+        nodeNum = len(inNI[node].split(';')[2].split('/'))
+        # print (nodeNum)
+        for i in range(0, nodeNum):
+            currNodeInfo = inNI[node].split(';')[2].split('/')[i]
+            currNodeName = currNodeInfo.split(',')[0]
+
+            if currNodeName != currNode:
+                currCoord = currNodeInfo.split(',')[1]
+                currNodeX = int(currCoord.split(' ')[0])
+                currNodeY = int(currCoord.split(' ')[1])
+                dist = math.sqrt((currNodeX - currX) ** 2 + (currNodeY - currY) ** 2)
+
+                # print dist
+                if dist <= radius:
+                    nodeInRTInfo = routingTable.get(nodeName, 'None')
+                    if nodeInRTInfo != 'None':
+                        currCost = int(currNodeInfo.split(',')[2]) + int(nodeInRTInfo.split(';')[1])
+                        currPath = nodeName + currNodeInfo.split(',')[3]
+                        updateNodeInfo(currNodeName, currCoord, currCost, currPath)
+
+
+# return
 
 
 def calCost(coord, nodeTime):
-	coordX = int(coord.split(' ')[0])
-	coordY = int(coord.split(' ')[1])
-	timehr = time.localtime(time.time()).tm_hour 
-	timemin = time.localtime(time.time()).tm_min
-	timesec = time.localtime(time.time()).tm_sec
-	currTime = 3600*timehr + 60*timemin + timesec
-	dTime = currTime - nodeTime
-	cost = int(alpha * (2**(math.sqrt((coordX - currX)**2) + (coordY - currY)**2)) + beta * dTime)
-	return cost
+    coordX = int(coord.split(' ')[0])
+    coordY = int(coord.split(' ')[1])
+    timehr = time.localtime(time.time()).tm_hour
+    timemin = time.localtime(time.time()).tm_min
+    timesec = time.localtime(time.time()).tm_sec
+    currTime = 3600 * timehr + 60 * timemin + timesec
+    dTime = currTime - nodeTime
+    cost = int(alpha * (2 ** (math.sqrt((coordX - currX) ** 2) + (coordY - currY) ** 2)) + beta * dTime)
+    return cost
 
 
 def updateNodeInfo(node, coord, cost, path):
-	global routingTable
-	isInRT = routingTable.get(node, 'None')
-	if isInRT == 'None':
-		routingTable[node] = coord + ';' + str(cost) + ';' + path
-	else:
-		routingCost = int(isInRT.split(';')[1])
-		routingCoord = isInRT.split(';')[0]
-		# print('cost:' + str(cost))
-		# print('routingCost:' + path + str(routingCost))
-		if routingCoord != coord:
+    global routingTable
+    isInRT = routingTable.get(node, 'None')
+    if isInRT == 'None':
+        routingTable[node] = coord + ';' + str(cost) + ';' + path
+    else:
+        routingCost = int(isInRT.split(';')[1])
+        routingCoord = isInRT.split(';')[0]
+        # print('cost:' + str(cost))
+        # print('routingCost:' + path + str(routingCost))
+        if routingCoord != coord:
+            routingTable[node] = coord + ';' + str(cost) + ';' + path
+        if routingCost > cost:
+            routingTable[node] = coord + ';' + str(cost) + ';' + path
 
-			routingTable[node] = coord + ';' + str(cost) + ';' + path
-		if routingCost > cost:
-			routingTable[node] = coord + ';' + str(cost) + ';' + path
+    return
 
-	return 
 
 def genHelloMsg():
-	global currSeq
-	timehr = time.localtime(time.time()).tm_hour 
-	timemin = time.localtime(time.time()).tm_min
-	timesec = time.localtime(time.time()).tm_sec
-	currTime = 3600*timehr + 60*timemin + timesec
-	currSeq += 1
-	data = "Hello;" + str(currSeq) +',' + str(currTime) + ';' + currNode + ',' + str(currX) + ' ' + str(currY) + ',' + currIP
-	if len(routingTable)>0:
-		data = data + ';'
-	for key in routingTable:
-		coord = routingTable[key].split(';')[0]
-		cost = routingTable[key].split(';')[1]
-		path = routingTable[key].split(';')[2]
-		data = data + key + ',' + coord + ',' + cost + ',' + path + '/'
-	data = data.rstrip('/')
-	return data
+    global currSeq
+    timehr = time.localtime(time.time()).tm_hour
+    timemin = time.localtime(time.time()).tm_min
+    timesec = time.localtime(time.time()).tm_sec
+    currTime = 3600 * timehr + 60 * timemin + timesec
+    currSeq += 1
+    data = "Hello;" + str(currSeq) + ',' + str(currTime) + ';' + currNode + ',' + str(currX) + ' ' + str(
+        currY) + ',' + currIP
+    if len(routingTable) > 0:
+        data = data + ';'
+    for key in routingTable:
+        coord = routingTable[key].split(';')[0]
+        cost = routingTable[key].split(';')[1]
+        path = routingTable[key].split(';')[2]
+        data = data + key + ',' + coord + ',' + cost + ',' + path + '/'
+    data = data.rstrip('/')
+    return data
+
 
 def scanSeq():
-	global routingTable
-	global ipTable
-	for key in seqT:
-		nodes = []
-		if (currSeq - int(seqT[key].split(' ')[1])) > 5:
-			for node in routingTable:
-				if routingTable[node].find(key) != -1:
-					nodes.append(node)
-			for element in nodes:
-				if routingTable.get(element, 'None') != 'None':
-					routingTable.pop(element)
-					ipTable.pop(element)
-			seqT[key] = str(0) + ' ' + str(currSeq)
-			
+    global routingTable
+    global ipTable
+    for key in seqT:
+        nodes = []
+        if (currSeq - int(seqT[key].split(' ')[1])) > 5:
+            for node in routingTable:
+                if routingTable[node].find(key) != -1:
+                    nodes.append(node)
+            for element in nodes:
+                if routingTable.get(element, 'None') != 'None':
+                    routingTable.pop(element)
+                    ipTable.pop(element)
+            seqT[key] = str(0) + ' ' + str(currSeq)
 
 
 def getEdge(source, destination):  # destination_node is a node outside the range (i.e. final destination)
@@ -282,7 +283,6 @@ def getEdge(source, destination):  # destination_node is a node outside the rang
         return edge
     else:
         return 'None'
-
 
 
 def createPath(destination):  # destination_node is a node in the range (i.e. edge node)
@@ -332,6 +332,7 @@ def recvAndTreatPkt():  # receive, check, print or route/send the packet
         elif currNode == packet['edge']:  # current node is the edge so calculate new route path in the range
             newpacket = genPkt(packet['source'], packet['destination'], currNode)
             if newpacket['edge'] != 'None':
+	    	newpacket['routingPath'] = packet['routingPath']
             	newpacket['routingPath'].append(currNode)
             	nextHop = newpacket['pathToEdge'].pop()
             	sendPkt(nextHop, newpacket)
@@ -359,38 +360,25 @@ def sendPkt(destination, packet):  # send packet to the next hop, packet is a di
     s.sendto(repr(packet), addr)
     s.close()
 
+
 def changeVariable():
-	global currX, currY
-	while True:
-		data = raw_input()
-		if data.find('coord:') != -1:
-			coord = data.split(':')[1]
-			if coord.find(' ') != -1:
-				x = coord.split(' ')[0]
-				y = coord.split(' ')[1]
-				if x.isdigit() and y.isdigit() :
-					currX = int(x)
-					currY = int(y)
-				else:
-					print 'Invalid Input'
-			else:
-				print 'Invalid Input'
-		else:
-			print 'Invalid Input'
-			
-def testSendPkt():
-	time.sleep(3)
-	coord_A = Coord(0,0)
-	coord_B = Coord(2,0)
-	coord_C = Coord(6,0)
-	nodeMap['A'] = coord_A
-	nodeMap['B'] = coord_B
-	nodeMap['C'] = coord_C
-	pkt = genPkt('A', 'C', 'A')
-	nexthop = pkt['pathToEdge'].pop()
-	pkt['routingPath'].append(currNode)
-	sendPkt(nexthop, pkt)
-	print 'SENDPKT#######################'
+    global currX, currY
+    while True:
+        data = raw_input()
+        if data.find('coord:') != -1:
+            coord = data.split(':')[1]
+            if coord.find(' ') != -1:
+                x = coord.split(' ')[0]
+                y = coord.split(' ')[1]
+                if x.isdigit() and y.isdigit():
+                    currX = int(x)
+                    currY = int(y)
+                else:
+                    print 'Invalid Input'
+            else:
+                print 'Invalid Input'
+        else:
+            print 'Invalid Input'
 	
 	
 	
