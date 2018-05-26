c00917563ac1a989d54dfca11cb54730e46d4572
EdgarHE <mingcheng.he@hotmail.com>
2018-05-26 22:35:40 +0800

    Update node.py

--- node.py
+++ node.py
@@ -200,7 +200,8 @@ def dealInNIMsg(node):
                     nodeInRTInfo = routingTable.get(nodeName, 'None')
                     if nodeInRTInfo != 'None':
                         currCost = int(currNodeInfo.split(',')[2]) + int(nodeInRTInfo.split(';')[1])
-                        currPath = nodeName + currNodeInfo.split(',')[3]
+			tempPath = routingTable(nodeName).split(';')[2]
+                        currPath = tempPath + currNodeInfo.split(',')[3]
 
                         isInRT = routingTable.get(currNodeName, 'None')
                         if isInRT != 'None':
